c47f6b13afed54c638cdb6b0bca95cbae7cfc9fc
hainguyenhuy2002 <81941497+hainguyenhuy2002@users.noreply.github.com>
2023-04-22 00:54:59 +0700

    updated

--- socials/crew3_users_crawler.py
+++ socials/crew3_users_crawler.py
@@ -120,7 +120,7 @@ class Crew3UserCrawler:
                         except Exception as ex:
                             logger.exception(ex)
                         finally:
-                            time.sleep(0.000001)
+                            time.sleep(0.00001)
                     t+=1
                 users.update(data)
                 # exporter.update_users(data)
