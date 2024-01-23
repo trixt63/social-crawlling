677244b8928ba8d15c30851c467c8b9902afd6c5
Trinh Xuan Tung <xuantung.trinh.63@gmail.com>
2024-01-23 15:18:49 +0700

    add flag to get new_community file

--- cli/crawl_zealy.py
+++ cli/crawl_zealy.py
@@ -12,16 +12,16 @@ logger = get_logger('Crawl social account')
 @click.command(context_settings=dict(help_option_names=['-h', '--help']))
 @click.option('-o', '--output', type=str, default=None, help='User social DB')
 @click.option('-od', '--output-database', type=str, default=None, help='DB Database name')
+@click.option( '--new-community', is_flag=True, help='Get new community file')
 @click.option('-f', '--file', type=str, default='data/zealy_communities.json', show_default=True,
-              help='File path')
+              help='Communities file path')
 @click.option('-b', '--batch-size', default=50, show_default=True, type=int, help='Batch size')
-def crawl_zealy(output, output_database, file, batch_size):
+def crawl_zealy(output, output_database, new_community, file, batch_size):
     db = SocialUsersDB(connection_url=output, database=output_database)
     crawler = ZealyUserCrawler(batch_size=batch_size,
                                communities_file=file,
                                database=db)
-    # crawler_old = Crew3UserCrawler_old()
 
-    # crawler.get_top_communities()
+    if new_community:
+        crawler.get_top_communities()
     crawler.get_users()
-    # crawler_old.get_users()
